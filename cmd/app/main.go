6eda85214ee830f5fe5606c0254a5b96c107bbc4
chiiriill <chiiriill@yandex.ru>
2026-05-07 21:40:45 +0300

    Complete

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("payment service started")
+	app := fiber.New()
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"status": "ok"})
+	})
+
+	log.Fatal(app.Listen(":8080"))
 }
